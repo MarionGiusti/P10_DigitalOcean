ca55741a58783f188c19bc9e75afa394966b5dbc
MarionGiusti <marion.giusti9@gmail.com>
2021-02-11 15:00:09 +0100

    create travis config

---
+++ purbeurre_project/settings/travis.py
@@ -0,0 +1,12 @@
+from .defaults import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    },
+}
